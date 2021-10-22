1f912b1ab88500319c9adf4e44c5004f5547ad4e
Angelo Pirola <angelo9871@gmail.com>
2021-10-22 14:56:33 +0200

    Aggiunto progetto .NET Core API with JWT

---
+++ src/API-Protect-With-JWT/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace API_Protect_With_JWT
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webHostBuilder => {
+                    webHostBuilder.UseStartup<Startup>();
+                });
+    }
+}
