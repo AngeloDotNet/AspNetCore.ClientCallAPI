349be4e09a3ba6c0122b78b9f1e78c62ea38d492
Angelo Pirola <angelo9871@gmail.com>
2026-01-19 12:35:52 +0100

    Update LoginInputModel to use PascalCase properties
    Renamed 'username' and 'password' to 'Username' and 'Password' to follow C# naming conventions. Reformatted class for improved clarity and removed unnecessary indentation.

--- src/AspNetCore-Client-CallAPI/Models/InputModels/LoginInputModel.cs
+++ src/AspNetCore-Client-CallAPI/Models/InputModels/LoginInputModel.cs
@@ -1,9 +1,8 @@
-namespace AspNetCore_Client_CallAPI.Models.InputModels
-{
-    public class LoginInputModel
-    {
-        public string username { get; set; }
+namespace AspNetCore_Client_CallAPI.Models.InputModels;
 
-        public string password { get; set; }
-    }
+public class LoginInputModel
+{
+    public string Username { get; set; }
+
+    public string Password { get; set; }
 }
